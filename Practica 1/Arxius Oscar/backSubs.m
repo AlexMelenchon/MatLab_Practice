f39074fc21199a4dcb4f93de451dcd4b755593db
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2019-10-07 19:20:08 +0200

    Lab 1 ex 2

--- Practica 1/Arxius Oscar/backSubs.m
+++ Practica 1/Arxius Oscar/backSubs.m
@@ -15,9 +15,9 @@ x(end)=b(end)/A(end,end);%%last case
 
 for t=arraysize(1)-1:-1:1
     
-    o=A(t,1+t:arraysize(1));%
-    p=x(t+1:arraysize(1));
-    
+    o=A(t,1+t:arraysize(1));%%takes the matrix A and takes the vector row from pivot +1
+    p=x(t+1:arraysize(1));%%takes the column of already resolved equations from the X vector
+
     x(t)=(b(t)-(o*p))/A(t,t);
 end
 
