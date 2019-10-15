de15099d5d6fc702f7c7b4b87223185f9d5588db
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2019-10-15 12:37:43 +0200

    He preparat el document de test

--- Practica 1/Arxius Oscar/backSubs.m
+++ Practica 1/Arxius Oscar/backSubs.m
@@ -1,12 +1,5 @@
 function [x] = backSubs(A,b)
-% [x] = backSubs(A,b)
-% Performs a backsubstitution process over the matrix [A b]
-%	to find x such that A*x = b with A upper triangular
-% Inputs:
-%	A: Upper triangular coefficients matrix
-%	b: Independent term
-% Outputs:
-%	x: Solution of A*x = b
+
 arraysize=size(A);
 
 x=zeros(arraysize(2),1);
--- Practica 1/Arxius Oscar/ownTriangulation.m
+++ Practica 1/Arxius Oscar/ownTriangulation.m
@@ -1,13 +1,4 @@
 function [At, bt]= ownTriangulation(A,b)
-% [At,bt] = ownTriangulation(A,b)
-% Performs a forward elimination proces over the matrix [A b]
-%	such that A*x = b and At*x = bt share the same solution x
-% Inputs:
-%	A: Coefficients matrix
-%	b: Independent term
-% Outputs:
-%	At: Upper triangular matrix
-% 	bt: Modified independent term
 
 arraysize=size(A);
 r=zeros(arraysize(1),arraysize(2)+1);
