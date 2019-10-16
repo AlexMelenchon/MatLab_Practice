ec065bb0033935011d25c46872b5faa0c0b06f5a
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2019-10-16 17:15:25 +0200

    OwnPivoting is now commented

--- Practica 1/Final/ownTriangulation.m
+++ Practica 1/Final/ownTriangulation.m
@@ -8,13 +8,27 @@ function [At, bt]= ownTriangulation(A,b)
 arraysize=size(A);
 r=[A b];
 
-%Loop as many times as the matrix allows us in order to do
 
-%Substitute the subpivot row for itself  multiplied for the pivot - the
-%pivot row multiplied for the subpivot.
+% In order to triangulate the matrix, we have to declarate a pivot and a
+% subpivot then, we substitute the subpivot row for itself multiplied for
+% the pivot - the pivot row multiplied for the subpivot.
+
+%Once this process is finished we do the same thing until we ran out of
+%rows or columns.
+
+%Knowing that, in order to do the traingulation, the pivot will be the cell
+%that is located at the number of columns and rows equal to our number of
+%loops, starting from 1. And, that the subpivot is the number at the same
+%column number of the pivot, just one row below.
+
+
+
+%Loop as many times as the matrix allows us in order to do
 for p=1:arraysize(1)
 for j=p+1:arraysize(1)
 r(j,p:end)=r(p,p)*r(j,p:end)-r(j,p)*r(p,p:end);
+%Substitute the subpivot row for itself  multiplied for the pivot - the
+%pivot row multiplied for the subpivot.
 
 end
 end
