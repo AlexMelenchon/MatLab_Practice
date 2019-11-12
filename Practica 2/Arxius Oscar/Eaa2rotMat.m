cbfd196e1563293cca7a0d2158543ad110894df4
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2019-11-12 18:51:05 +0100

    Lab02 Ex1 Solved

--- Practica 2/Arxius Oscar/Eaa2rotMat.m
+++ Practica 2/Arxius Oscar/Eaa2rotMat.m
@@ -7,5 +7,17 @@ function [R] = Eaa2rotMat(a,u)
 % Outputs:
 %	R: generated rotation matrix
 
+u = u(:);
+
+u = u/sqrt(u'*u);
+
+identity = eye(3);
+
+UX = [0,-u(3), u(2);
+        u(3), 0, -u(1);
+        -u(2), u(1), 0];
+
+R = identity + (sind(a)*UX) + (1-cosd(a))*UX^2
+
 end
 
