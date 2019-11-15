7d75e9960fa8c5777b11bd991daa70bdc79d0c7e
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2019-11-15 20:07:16 +0100

    Exercise 3 Posible Solution

--- Practica 2/Arxius Oscar/rotMat2Eaa.m
+++ Practica 2/Arxius Oscar/rotMat2Eaa.m
@@ -2,11 +2,17 @@ function [a,u] = rotMat2Eaa(R)
 % [a,u] = rotMat2Eaa(R)
 % Computes the angle and principal axis of rotation given a rotation matrix R. 
 % Inputs:
-%	R: rotation matrix
+%    R: rotation matrix
 % Outputs:
-%	a: angle of rotation
-%	u: axis of rotation 
+%    a: angle of rotation in degrees
+%    u: axis of rotation 
+trace=R(1,1)+R(2,2)+R(3,3);
+a=acosd((trace-1)/2);
 
-
+if(mod(a,180)~=0)%if the angle in radians is multiple of pi(180 degrees), the rotation vector will be (0,0,0)
+Ux=(R-transpose(R))/(2*sind(a));
+u=[Ux(3,2);Ux(1,3);Ux(2,1)];
+else
+    u=[0;0;0];
 end
-
+end
