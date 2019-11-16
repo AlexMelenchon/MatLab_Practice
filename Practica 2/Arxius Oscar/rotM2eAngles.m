1dcf932bc41cba3772ac2b0e440bb715fcab8207
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2019-11-16 17:25:48 +0100

    LAB02 is DONE

--- Practica 2/Arxius Oscar/rotM2eAngles.m
+++ Practica 2/Arxius Oscar/rotM2eAngles.m
@@ -2,34 +2,27 @@ function [yaw, pitch, roll] = rotM2eAngles(R)
 % [yaw, pitch, roll] = rotM2eAngles(R)
 % Computes the Euler angles (yaw, pitch, roll) given an input rotation matrix R.
 % Inputs:
-%	R: rotation matrix
+%    R: rotation matrix
 % Outputs:
-%	yaw: angle of rotation around the z axis
-%	pitch: angle of rotation around the y axis
-%	roll: angle of rotation around the x axis
+%    yaw: angle of rotation around the z axis
+%    pitch: angle of rotation around the y axis
+%    roll: angle of rotation around the x axis
 
 pitch = asind(-R(3,1));
 
 if(sind(pitch) == 1 || sind(pitch) == -1)
-  
+
     roll = 0; 
-    
+
     if(sind(pitch) == 1)
-        yaw = ;
+        yaw = roll + atan2d((R(1,2)), (R(2,2)));
     end
-    if ()sind(pitch) == -1)
-        yaw = ;
+    if (sind(pitch) == -1)
+        yaw = roll + -atan2d(R(1,2), R(2,2));
     end
-    
-end
-
 else
-roll = atan2(R(3,2)/ cosd(pitch), R(3,3)/ cosd(pitch)) * 180 / pi;
-yaw = atan2(R(2,1)/ cosd(pitch), R(1,1)/ cosd(pitch)) * 180 / pi;
+roll = atan2d(R(3,2)/ cosd(pitch), R(3,3)/ cosd(pitch));
+yaw = atan2d(R(2,1)/ cosd(pitch), R(1,1)/ cosd(pitch));
 end
 
-end
-
-
-
-
+ end
