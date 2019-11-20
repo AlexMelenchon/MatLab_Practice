6d429af037a5a6afa44179b217358efc40a3cfdb
AlexMelenchon <47743805+AlexMelenchon@users.noreply.github.com>
2019-11-20 20:12:27 +0100

    MatLab Still Fighting w nº4 & nº3

--- Practica 2/Arxius Oscar/rotMat2Eaa.m
+++ Practica 2/Arxius Oscar/rotMat2Eaa.m
@@ -10,10 +10,21 @@ function [a,u] = rotMat2Eaa(R)
 trace=R(1,1)+R(2,2)+R(3,3);
 a=acosd((trace-1)/2);
 
-if(mod(a,180)~=0)%if the angle in radians is multiple of pi(180 degrees), the rotation vector will be (0,0,0)
+if(mod(a,180)~=0)
+    
 Ux=(R-transpose(R))/(2*sind(a));
 u=[Ux(3,2);Ux(1,3);Ux(2,1)];
+
 else
-    u=[0;0;0];
+    
+    if (a == 0)
+    u=[0;0;0]; % If the angle is 0, the axis is irrelevant
+    end
+    
+    if (a == 180)
+     u = [sqrt((R(3,3)+1)/2), sqrt((R(2,2)+1)/2), sqrt((R(1,1)+1)/2)];
+    end
+    
 end
+
 end
