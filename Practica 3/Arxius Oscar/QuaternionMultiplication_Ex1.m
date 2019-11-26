afdd01d4e5763d74b11fc1b29ffb8c109a09c7af
oscarpm5 <47557468+oscarpm5@users.noreply.github.com>
2019-11-26 18:39:51 +0100

    Lab_03 Exercise 1 solved

--- Practica 3/Arxius Oscar/QuaternionMultiplication_Ex1.m
+++ Practica 3/Arxius Oscar/QuaternionMultiplication_Ex1.m
@@ -1,35 +1,35 @@
-function varargout = untitled(varargin)
-% UNTITLED MATLAB code for untitled.fig
-%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+function varargout = QuaternionMultiplication_Ex1(varargin)
+% QUATERNIONMULTIPLICATION_EX1 MATLAB code for QuaternionMultiplication_Ex1.fig
+%      QUATERNIONMULTIPLICATION_EX1, by itself, creates a new QUATERNIONMULTIPLICATION_EX1 or raises the existing
 %      singleton*.
 %
-%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      H = QUATERNIONMULTIPLICATION_EX1 returns the handle to a new QUATERNIONMULTIPLICATION_EX1 or the handle to
 %      the existing singleton*.
 %
-%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%      QUATERNIONMULTIPLICATION_EX1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in QUATERNIONMULTIPLICATION_EX1.M with the given input arguments.
 %
-%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
+%      QUATERNIONMULTIPLICATION_EX1('Property','Value',...) creates a new QUATERNIONMULTIPLICATION_EX1 or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before untitled_OpeningFcn gets called.  An
+%      applied to the GUI before QuaternionMultiplication_Ex1_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%      stop.  All inputs are passed to QuaternionMultiplication_Ex1_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help untitled
+% Edit the above text to modify the response to help QuaternionMultiplication_Ex1
 
-% Last Modified by GUIDE v2.5 26-Nov-2019 17:46:44
+% Last Modified by GUIDE v2.5 26-Nov-2019 18:34:06
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
-                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_OpeningFcn', @QuaternionMultiplication_Ex1_OpeningFcn, ...
+                   'gui_OutputFcn',  @QuaternionMultiplication_Ex1_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before untitled is made visible.
-function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before QuaternionMultiplication_Ex1 is made visible.
+function QuaternionMultiplication_Ex1_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to untitled (see VARARGIN)
+% varargin   command line arguments to QuaternionMultiplication_Ex1 (see VARARGIN)
 
-% Choose default command line output for untitled
+% Choose default command line output for QuaternionMultiplication_Ex1
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes untitled wait for user response (see UIRESUME)
+% UIWAIT makes QuaternionMultiplication_Ex1 wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+function varargout = QuaternionMultiplication_Ex1_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
